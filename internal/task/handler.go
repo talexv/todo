cf7a234d7ba24555db4871193b3aa272222de13a
talexv <talexvi@yandex.ru>
2025-06-04 23:01:50 +0500

    Add basic implementation of todo app

---
+++ internal/task/handler.go
@@ -0,0 +1,125 @@
+package task
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+)
+
+type Handler struct {
+	DB *DB
+}
+
+func NewTaskHandler(router *http.ServeMux, db *DB) {
+	handler := &Handler{DB: db}
+
+	router.HandleFunc("GET /tasks", handler.GetTasks())
+	router.HandleFunc("POST /create", handler.CreateTask())
+	router.HandleFunc("PATCH /tasks/{id}/done", handler.UpdateStatusTask())
+	router.HandleFunc("DELETE /tasks/{id}/delete", handler.DeleteTask())
+}
+
+func (handler *Handler) GetTasks() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tasks, err := handler.DB.GetAllTasks(r.Context())
+		if err != nil {
+			http.Error(w, "ошибка при получении списка задач", http.StatusInternalServerError)
+			return
+		}
+
+		writeJSON(w, tasks, http.StatusOK)
+	}
+}
+
+func (handler *Handler) CreateTask() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+
+		var req Request
+
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			http.Error(w, "некорректный JSON", http.StatusBadRequest)
+			return
+		}
+
+		if req.Title == "" {
+			http.Error(w, "'title' обязательно к заполнению", http.StatusBadRequest)
+			return
+		}
+
+		task, err := handler.DB.InsertTask(r.Context(), req.Title)
+		if err != nil {
+			http.Error(w, "ошибка при создании задачи", http.StatusInternalServerError)
+			return
+		}
+
+		writeJSON(w, task, http.StatusCreated)
+	}
+}
+
+func (handler *Handler) DeleteTask() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id, err := parseID(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		err = handler.DB.DeleteTask(r.Context(), id)
+		if err != nil {
+			if errors.Is(err, ErrTaskNotFound) {
+				http.Error(w, "задача не найдена", http.StatusNotFound)
+				return
+			}
+
+			http.Error(w, "ошибка при удалении задачи", http.StatusInternalServerError)
+
+			return
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+	}
+}
+
+func (handler *Handler) UpdateStatusTask() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id, err := parseID(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		task, err := handler.DB.UpdateStatusTask(r.Context(), id)
+		if err != nil {
+			if errors.Is(err, ErrTaskNotFound) {
+				http.Error(w, "задача не найдена", http.StatusNotFound)
+				return
+			}
+
+			http.Error(w, "ошибка при обновлении статуса задачи", http.StatusInternalServerError)
+
+			return
+		}
+
+		writeJSON(w, task, http.StatusOK)
+	}
+}
+
+func writeJSON(w http.ResponseWriter, data any, statusCode int) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(statusCode)
+	_ = json.NewEncoder(w).Encode(data)
+}
+
+func parseID(r *http.Request) (int64, error) {
+	idStr := r.PathValue("id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+
+	if err != nil || id <= 0 {
+		return 0, errors.New("некорректный id")
+	}
+
+	return id, nil
+}
---
+++ internal/task/model.go
@@ -0,0 +1,143 @@
+package task
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type Task struct {
+	ID    int64  `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+}
+
+type Request struct {
+	Title string `json:"title"`
+}
+
+const schemaSQL = `
+	CREATE TABLE IF NOT EXISTS tasks (
+		id bigserial primary key,
+		title text NOT NULL,
+		done BOOLEAN default false
+	);
+`
+
+const getAllTasksSQL = `
+	SELECT id, title, done FROM tasks;
+`
+
+const createTaskSQL = `
+	INSERT INTO tasks (title)
+	VALUES ($1)
+	RETURNING id, title, done;
+`
+
+const updateStatusTaskSQL = `
+	UPDATE tasks
+	SET done = true
+	WHERE id = $1
+	RETURNING id, title, done;
+`
+
+const deleteTaskSQL = `
+	DELETE FROM tasks WHERE id = $1
+`
+
+var ErrTaskNotFound = errors.New("задача не найдена")
+
+type DB struct {
+	conn *pgx.Conn
+}
+
+func NewDB(connString string) (*DB, error) {
+	conn, err := pgx.Connect(context.Background(), connString)
+	if err != nil {
+		return nil, fmt.Errorf("NewDB: pgx.Connect: %w", err)
+	}
+
+	_, err = conn.Exec(context.Background(), schemaSQL)
+	if err != nil {
+		return nil, fmt.Errorf("NewDB: conn.Exec: %w", err)
+	}
+
+	return &DB{conn: conn}, nil
+}
+
+func (d *DB) CloseConn() error {
+	err := d.conn.Close(context.Background())
+	d.conn = nil
+
+	return err
+}
+
+func (d *DB) InsertTask(ctx context.Context, title string) (*Task, error) {
+	var task Task
+
+	err := d.conn.QueryRow(ctx, createTaskSQL, title).Scan(&task.ID, &task.Title, &task.Done)
+	if err != nil {
+		return nil, fmt.Errorf("InsertTask: conn.QueryRow: %w", err)
+	}
+
+	return &task, nil
+}
+
+func (d *DB) GetAllTasks(ctx context.Context) ([]*Task, error) {
+	rows, err := d.conn.Query(ctx, getAllTasksSQL)
+	if err != nil {
+		return nil, fmt.Errorf("GetAllTasks: conn.Query: %w", err)
+	}
+
+	defer rows.Close()
+
+	var tasks []*Task
+
+	for rows.Next() {
+		task := &Task{}
+
+		err = rows.Scan(&task.ID, &task.Title, &task.Done)
+		if err != nil {
+			return nil, fmt.Errorf("GetAllTasks: rows.Scan: %w", err)
+		}
+
+		tasks = append(tasks, task)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, fmt.Errorf("GetAllTasks: rows iteration error: %w", err)
+	}
+
+	return tasks, nil
+}
+
+func (d *DB) DeleteTask(ctx context.Context, id int64) error {
+	tag, err := d.conn.Exec(ctx, deleteTaskSQL, id)
+	if err != nil {
+		return fmt.Errorf("DeleteTask: conn.Exec: %w", err)
+	}
+
+	if tag.RowsAffected() == 0 {
+		return ErrTaskNotFound
+	}
+
+	return nil
+}
+
+func (d *DB) UpdateStatusTask(ctx context.Context, id int64) (*Task, error) {
+	var task Task
+
+	err := d.conn.QueryRow(ctx, updateStatusTaskSQL, id).Scan(&task.ID, &task.Title, &task.Done)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrTaskNotFound
+		}
+
+		return nil, fmt.Errorf("UpdateStatusTask: conn.QueryRow: %w", err)
+	}
+
+	return &task, nil
+}
